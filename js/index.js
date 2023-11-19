e41e8a402b8700a4033fc9f4f8515b563b01bf8c
Gabriela Ricarte <gabrielagricarte1@gmail.com>
2023-11-19 16:13:29 -0300

    dark theme add

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const barBtn = document.getElementById("bar");
 const show = document.querySelector(".show-items")
 const feelingLuckyBtn = document.querySelector("#luckyBtn");
+const darkModeBtn = document.getElementById("darkMode");
+const container = document.querySelector(".container");
 
 barBtn.addEventListener('click', function() {
     if (show) {
@@ -8,3 +10,20 @@ barBtn.addEventListener('click', function() {
         show.style.display = isMenuVisible ? 'none' : 'flex';
     }
 });
+
+darkModeBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    container.classList.toggle('dark');
+    updateDarkModeText();
+});
+
+function updateDarkModeText() {
+    const darkModeText = document.getElementById("darkMode");
+    const isDarkMode = container.classList.contains('dark');
+    
+    if (isDarkMode) {
+        darkModeText.innerHTML = '<a href="#">Dark Theme: activated</a>';
+    } else {
+        darkModeText.innerHTML = '<a href="#">Dark Theme: deactivated</a>';
+    }
+}
