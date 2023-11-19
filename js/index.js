c58714ce354261e1a69e882643730331950b7d54
Gabriela Ricarte <gabrielagricarte1@gmail.com>
2023-11-18 21:06:43 -0300

    footer add

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const barBtn = document.getElementById("bar");
 const show = document.querySelector(".show-items")
+const feelingLuckyBtn = document.querySelector("#luckyBtn");
 
 barBtn.addEventListener('click', function() {
     if (show) {
